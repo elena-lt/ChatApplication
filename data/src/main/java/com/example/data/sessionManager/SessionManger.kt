506116e7bf19ba0fdaf60ac5463a69404ef52667
Elena Lutsiuk <elenalutsiuk5@gmail.com>
2021-09-05 15:09:28 +0200

    logout fun updated-force clear local db

--- data/src/main/java/com/example/data/sessionManager/SessionManger.kt
+++ data/src/main/java/com/example/data/sessionManager/SessionManger.kt
@@ -4,15 +4,18 @@ import android.content.SharedPreferences
 import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
+import com.example.data.persistance.AppDatabase
 import com.example.data.utils.Const.SP_USER_ID
 import com.example.data.utils.Const.SP_USER_LOGIN
 import com.quickblox.auth.session.QBSession
 import com.quickblox.auth.session.QBSessionManager
 import com.quickblox.auth.session.QBSessionParameters
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 class SessionManger @Inject constructor(
     private val qbSessionManager: QBSessionManager,
+    private val database: AppDatabase,
     private val sharedPreferences: SharedPreferences,
     val sharedPreferencesEditor: SharedPreferences.Editor
 ) {
@@ -21,37 +24,46 @@ class SessionManger @Inject constructor(
     val currUser: LiveData<String?> = _currUser
 
     val listener = SharedPreferences.OnSharedPreferenceChangeListener { sharedPreferences, key ->
-        Log.d("AppDebug", "listener: some data changed ${key}")
         when (key) {
             SP_USER_LOGIN -> {
-                Log.d("AppDebug", "listener: SP_USER_LOGIN data changed")
-                _currUser.value = sharedPreferences.getString(SP_USER_LOGIN, null)
+                Log.d("AppDebug", "listener: new value set  ")
+                _currUser.postValue(sharedPreferences.getString(SP_USER_LOGIN, null))
             }
         }
     }
 
     fun login(userId: Int, userLogin: String) {
-        Log.d("AppDebug", "login: apply $userId, $userLogin")
         val prevUserId = sharedPreferences.getInt(SP_USER_ID, -1)
         val prevUserLogin = sharedPreferences.getString(SP_USER_LOGIN, null)
 
-        if (userId != prevUserId) {
-            sharedPreferencesEditor.putInt(SP_USER_ID, userId).apply()
-        }
-        if (userLogin != prevUserLogin) {
-            sharedPreferencesEditor.putString(SP_USER_LOGIN, userLogin).apply()
-        }
+        sharedPreferencesEditor.putString(SP_USER_LOGIN, userLogin).apply()
+        sharedPreferencesEditor.putInt(SP_USER_ID, userId).apply()
+
+//        if (userId != prevUserId) {
+//            sharedPreferencesEditor.putInt(SP_USER_ID, userId).apply()
+//        }
+//        if (userLogin != prevUserLogin) {
+//            sharedPreferencesEditor.putString(SP_USER_LOGIN, userLogin).apply()
+//        }
     }
 
     fun logout() {
-        Log.d("AppDebug", "logout")
-        sharedPreferencesEditor.clear().apply()
-//        sharedPreferencesEditor.putString(SP_USER_LOGIN, null).apply()
-//        sharedPreferencesEditor.putString(SP_ACCESS_TOKEN, null).apply()
+        CoroutineScope(Dispatchers.IO).launch {
+            launch {
+                val job = this.launch {
+                    database.clearAllTables()
+                }
+
+                job.cancelAndJoin()
+                withContext(Dispatchers.Main){
+                    sharedPreferencesEditor.putString(SP_USER_LOGIN, null).apply()
+                    sharedPreferencesEditor.putString(SP_USER_ID, null).apply()
+                }
+            }
+        }
     }
 
     fun registerListener() {
-        Log.d("AppDebug", "registerListener: listener registered")
         sharedPreferences.registerOnSharedPreferenceChangeListener(listener)
     }
 
@@ -63,7 +75,7 @@ class SessionManger @Inject constructor(
         qbSessionManager.addListener(object : QBSessionManager.QBSessionListener {
             override fun onSessionCreated(session: QBSession) {
                 // calls when session was created firstly or after it has been expired
-                Log.d("SESSION_MANAGER", "onSessionCreated: ")
+                Log.d("AppDebug", "SESSION CREATED: ${session.userId} ")
             }
 
             override fun onSessionUpdated(sessionParameters: QBSessionParameters) {
@@ -74,38 +86,29 @@ class SessionManger @Inject constructor(
                 val userId = sessionParameters.userId
                 login(userId, userLogin)
 
-                Log.d(
-                    "SESSION_MANAGER",
-                    "onSessionUpdated: ${sessionParameters.accessToken}, ${sessionParameters.userLogin}"
-                )
+                Log.d("AppDebug", "SESSION UPDATED: $userLogin")
             }
 
             override fun onSessionDeleted() {
                 // calls when user signed Out or session was deleted
+                Log.d("AppDebug", "SESSION DELETED")
                 logout()
 
             }
 
             override fun onSessionRestored(session: QBSession) {
                 // calls when session was restored from local storage
-                login(
-                    qbSessionManager.sessionParameters.userId,
-                    qbSessionManager.sessionParameters.userLogin
-                )
-                Log.d(
-                    "SESSION_MANAGER",
-                    "onSessionRestored: ${session.token}, ${qbSessionManager.sessionParameters.userLogin}"
-                )
+                Log.d("AppDebug", "SESSION RESTORED ${session.userId}")
             }
 
             override fun onSessionExpired() {
                 // calls when session is expired
-                Log.d("SESSION_MANAGER", "onSessionExpired: ")
+                Log.d("AppDebug", "onSessionExpired: ")
             }
 
             override fun onProviderSessionExpired(provider: String) {
                 // calls when provider's access token is expired or invalid
-                Log.d("SESSION_MANAGER", "onProviderSessionExpired: ")
+                Log.d("AppDebug", "onProviderSessionExpired: ")
             }
         })
     }
